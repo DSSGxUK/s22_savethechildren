7fc5b152909c0157656db6739e294afccc9717e2
John Fitzgerald <j.a.fitzgerald@outlook.com>
2022-07-04 18:49:49 +0100

    notebook for first clean data production

--- src/stc_unicef_cpi/data/process_geotiff.py
+++ src/stc_unicef_cpi/data/process_geotiff.py
@@ -31,9 +31,14 @@ def geotiff_to_df(geotiff_filepath):
     """
     # # NB quadkeys are defined on Mercator projection, so must reproject
     # world = world.to_crs("EPSG:3395")  # world.to_crs(epsg=3395) would also work
+    reproj = False
     with rxr.open_rasterio(geotiff_filepath, masked=True) as open_file:
         name = Path(geotiff_filepath).name
         print_tif_metadata(open_file, name)
+        if open_file.rio.crs != "EPSG:4326":
+            print("Reprojection to lat/lon required: completing...")
+            reproj = True
+            og_proj = open_file.rio.crs
         open_file = open_file.squeeze()
         open_file.name = "data"
         band_names = open_file.attrs["long_name"]
@@ -42,7 +47,41 @@ def geotiff_to_df(geotiff_filepath):
     df.drop(columns=["spatial_ref"], inplace=True)
     df.dropna(subset=["data"], inplace=True)
     df.rename(index=dict(zip(range(1, len(band_names) + 1), band_names)), inplace=True)
-    df.index.set_names(["band", "latitude", "longitude"], inplace=True)
+
+    if len(df.index.names) == 3:
+        df.index.set_names(["band", "latitude", "longitude"], inplace=True)
+    elif len(df.index.names) == 2:
+        df.index.set_names(["latitude", "longitude"], inplace=True)
+    else:
+        raise ValueError("Unexpected number of index levels")
+    # Now converted to csv with band/lat/lon index
+    # So unstack band index into separate columns if multband,
+    # and drop unhelpful column multiindex
+    if len(df.index.names) == 3:
+        df = df.unstack(level=0).droplevel(0, axis=1).reset_index()
+    else:
+        # Single band, but check to make sure, then rename data
+        # suitably
+        nbands = df.band.nunique()
+        title = name.lstrip("cpi").rstrip(".tif")
+        print(f"{nbands} bands found in {title}")
+        if nbands > 1:
+            raise ValueError("More than one band, need to handle")
+        else:
+            df.drop(columns=["band"], inplace=True)
+        df.rename(columns={"data": title.strip("Data")}, inplace=True)
+        df = df.reset_index()
+
+    if reproj:
+        # do reprojection of coords to lat/lon
+        from pyproj import Transformer
+
+        transformer = Transformer.from_crs(og_proj, "EPSG:4326")
+        coords = [
+            transformer.transform(x, y) for x, y in df[["latitude", "longitude"]].values
+        ]
+        df[["latitude", "longitude"]] = coords
+
     # print(df.head())
     return df
 
