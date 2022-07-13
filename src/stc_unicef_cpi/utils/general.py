970176557ea20515a9a0c2ad22e23d95274ab21c
Daniela Pinto Veizaga <danielapintoveizaga@gmail.com>
2022-07-13 18:10:58 -0400

    creds fb

--- src/stc_unicef_cpi/utils/general.py
+++ src/stc_unicef_cpi/utils/general.py
@@ -2,9 +2,34 @@
 import wget
 import zipfile
 import os
+import yaml
 
 from functools import wraps
 from time import time
+
+
+def read_yaml_file(yaml_file):
+    """Load yaml configurations"""
+    config = None
+    try:
+        with open(yaml_file, "r") as f:
+            config = yaml.safe_load(f)
+    except:
+        raise FileNotFoundError("Couldnt load the file")
+
+    return config
+
+
+def get_facebook_credentials(creds_file):
+    """Get credentials for accessing FB API from the credentials file"""
+    creds = read_yaml_file(creds_file)["facebook"]
+    token = creds["access_token"]
+    id = creds["account_id"]
+    limit = creds["limit"]
+    radius = creds["radius"]
+    optimization = creds["optimization"]
+
+    return token, id, limit, radius, optimization
 
 
 def download_file(url, name):
