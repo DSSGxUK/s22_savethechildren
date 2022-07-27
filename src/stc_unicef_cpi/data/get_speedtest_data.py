3131dd8064e6edd2795a58ea6954a2ee3f581cd8
Daniela Pinto Veizaga <danielapintoveizaga@gmail.com>
2022-07-27 16:08:27 -0400

    formating stuff

--- src/stc_unicef_cpi/data/get_speedtest_data.py
+++ src/stc_unicef_cpi/data/get_speedtest_data.py
@@ -1,9 +1,17 @@
+import geopandas as gpd
 
 from datetime import datetime
-import geopandas as gpd
 
 
 def get_speedtest_url(service_type, year, q):
+    """Get Speed Test Url From Ookla
+    :param service_type: type of network performance
+    :type service_type: str
+    :param year: year
+    :type year: int
+    :param q: quarter
+    :type q: int
+    """
 
     def quarter_start(year, q):
         if not 1 <= q <= 4:
@@ -16,25 +24,33 @@ def get_speedtest_url(service_type, year, q):
 
     base_url = "https://ookla-open-data.s3.amazonaws.com/shapefiles/performance"
     url = f"{base_url}/type={service_type}/year={dt:%Y}/quarter={q}/{dt:%Y-%m-%d}_performance_{service_type}_tiles.zip"
-    name = f"{dt:%Y-%m-%d}_performance_{service_type}_tiles.zip"
+    name = f"{dt:%Y-%m-%d}_performance_{service_type}_tiles.csv"
     return url, name
 
 
-def prep_tile(data):
+def prep_tile(data, name):
+    """Prepare tile
+
+    _extended_summary_
+
+    :param data: _description_
+    :type data: _type_
+    :param name: _description_
+    :type name: _type_
+    """
     data = data[['avg_d_kbps', 'avg_u_kbps', 'geometry']]
-    print(data)
-    print('Writing country speedtest data to directory...')
-    data.to_csv('data_speedtest.csv', index=False)
+    print('Saving speedtest data to directory...')
+    data.to_csv(name, index=False)
 
 
-def get_speedtest_info(url):
+def get_speedtest_info(url, name):
         try:
             gdf_tiles = gpd.read_file(url)
             data = gdf_tiles
-            data = prep_tile(data)
+            data = prep_tile(data, name)
         except:
-            pass
+            raise ValueError("Unable to retrieve data.")
 
 
 url, name = get_speedtest_url(service_type='mobile', year=2021, q=4)
-get_speedtest_info(url)
+get_speedtest_info(url, name)
