71530f15b83f42bb294f0ba973988fa85454961b
Arpita <arpita.saggar@gmail.com>
2022-08-15 06:34:33 +0530

    Clean code and add notebook

--- src/stc_unicef_cpi/models/prediction_intervals.py
+++ src/stc_unicef_cpi/models/prediction_intervals.py
@@ -15,8 +15,8 @@ def calibrate_prediction_intervals(pipeline_dir, pipeline_name, input_data, targ
     Inputs:
         pipeline_dir: path to saved pipeline (from train_model.py)
         pipeline_name: name of saved pkl file
-        input_data: Dataframe containing training data, including target variable
-        target_dim: one of 6 dimensions from education, health, sanitation, housing, nutrition, water
+        input_data: Dataframe containing all data; output of make_dataset
+        target_dim: dimension to predict
         mapie_dir: path to save fitted MapieRegressor instance
 
     Outputs: None
@@ -28,9 +28,13 @@ def calibrate_prediction_intervals(pipeline_dir, pipeline_name, input_data, targ
     if not fitted:
         print("Warning, pipeline has not been fitted yet, cannot be used to generate intervals!")
         return
+
+    # subset by pipeline input
+    input_data = input_data[list(np.append(pipeline.feature_names_in_, target_dim))]
     
-    y_col = "dep_" + target_dim + "_sev"
-    x, y = input_data.loc[:, input_data.columns != y_col], input_data.loc[:, y_col]
+    # subset training rows with ground truth
+    input_data = input_data[~input_data[target_dim].isnull()]
+    x, y = input_data.loc[:, input_data.columns != target_dim], input_data.loc[:, target_dim]
 
     mapie = MapieRegressor(estimator=pipeline, cv="prefit").fit(x, y)
     joblib.dump(mapie, Path(mapie_dir)/("mapie_"+target_dim+".pkl"))
@@ -42,25 +46,31 @@ def predict_intervals(input_data, target_dim, mapie_dir, alpha=0.05, save_dir=No
     Get prediction intervals for all data using fitted MapieRegressor
 
     Inputs:
-        input_data: Dataframe containing data for which intervals are needed; may/may not include target
+        input_data: Dataframe containing all data; output of make_dataset
+        target_dim: dimension to predict
+        mapie_dir: path to saved MapieRegressor instance
+        alpha: percent of out of intervals predictions tolerated
+        save_dir: path to save predictions csv
 
     Outputs: If save_dir is None, Dataframe of shape (input_dir.shape[0], 3), else None
     """
 
     mapie = joblib.load(Path(mapie_dir)/("mapie_"+target_dim+".pkl"))
 
-    y_col = "dep_"+target_dim+"_sev"
-    if y_col in input_data.columns:
-        x = input_data.loc[:, input_data.columns != y_col]
-    else:
-        x = input_data
+    # subset by pipeline input
+    input_data = input_data[list(mapie.estimator.feature_names_in_)]
 
-    pred_intervals = pd.DataFrame(mapie.predict(x, alpha=alpha)[1].reshape(-1,2), columns=["lower", "upper"])
-    pred_intervals["upper"] = pred_intervals["upper"].apply(lambda x: 1 if x > 1 else x)
-    pred_intervals["lower"] = pred_intervals["lower"].apply(lambda x: 0 if x < 0 else x)
+    pred_intervals = pd.DataFrame(
+        mapie.predict(input_data, alpha=alpha)[1].reshape(-1,2),
+        columns=["lower_"+target_dim, "upper_"+target_dim]
+    )
+    
+    # restrict upper & lower bound for predictions to 1 & 0 resp
+    pred_intervals["upper_"+target_dim] = pred_intervals["upper_"+target_dim].apply(lambda x: 1 if x > 1 else x)
+    pred_intervals["lower_"+target_dim] = pred_intervals["lower_"+target_dim].apply(lambda x: 0 if x < 0 else x)
 
-    pred_intervals["prediction"] = mapie.predict(x)
-    pred_intervals = pred_intervals[["lower", "prediction", "upper"]]
+    pred_intervals["prediction_"+target_dim] = mapie.predict(input_data)
+    pred_intervals = pred_intervals[["lower_"+target_dim, "prediction_"+target_dim, "upper_"+target_dim]]
 
     if save_dir:
         pred_intervals.to_csv(Path(save_dir)/("intervals_"+target_dim+".csv"), index=False)
